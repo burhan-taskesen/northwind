93660bfe0178eceb4f93469814a4ecff4d94191c
Burhan <taskesenburhan@gmail.com>
2022-06-19 11:51:42 +0300

    makro denemesi

--- src/main/java/com/nortwind/app/entities/concretes/Product.java
+++ src/main/java/com/nortwind/app/entities/concretes/Product.java
@@ -1,4 +1,18 @@
 package com.nortwind.app.entities.concretes;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+    private int id;
+    private int categoryId;
+    private String productName;
+    private double unitPrice;
+    private int unitsInStock;
+    private String quantityPerUnit;
+
 }
